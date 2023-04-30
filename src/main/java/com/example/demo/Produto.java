8d123cf54e7d9b392a411a526a7ae14fc3ec0902
apazze <apazze@users.noreply.github.com>
2023-04-30 13:16:36 -0300

    Template inicial

---
+++ src/main/java/com/example/demo/Produto.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+//@Entity
+////@Table(name = "produto")
+//public class Produto {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//}
