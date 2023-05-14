8c938854da047e080694a494e1c6026d51edff88
apazze <apazze@users.noreply.github.com>
2023-05-14 20:01:25 -0300

    Uso de paginação e ordenação

--- src/main/java/com/example/demo/controller/PessoaController.java
+++ src/main/java/com/example/demo/controller/PessoaController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.relacionamentos.Categoria;
+import com.example.demo.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PessoaController {
+
+    @Autowired
+    CategoriaService categoriaService;
+
     @PostMapping("/bla")
     @ResponseStatus(HttpStatus.OK)
-    public void cadastro(){
-
+    public Page<Categoria> cadastro(Pageable pageable) {
+        return categoriaService.Pesquisar(pageable);
     }
 }
--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import com.example.demo.relacionamentos.Categoria;
 import com.example.demo.relacionamentos.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +13,14 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public void add() {
-        categoriaRepository.save(new Categoria());
+    public Page<Categoria> Pesquisar(Pageable pageable) {
+        for (int i = 0; i < 100; i++) {
+            Categoria categoria = new Categoria();
+            categoria.setNome("Categoria_" + i+1);
+            categoriaRepository.save(categoria);
+        }
+
+        Page<Categoria> all = categoriaRepository.findAll(pageable);
+        return all;
     }
 }
